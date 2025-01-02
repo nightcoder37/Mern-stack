08d687cafff5b3eea8e19f50756ec4a76f007204
Hassan-shah9 <hassan.thepearlsystem@gmail.com>
2025-01-02 09:12:18 +0700

    add prettier and app ts make server

--- server.ts
+++ server.ts
@@ -1,1 +1,10 @@
-console.log('ebook Api');
+import app from "./src/app";
+
+const startServer = () => {
+  const port = process.env.PORT || 3000;
+
+  app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+  });
+};
+startServer();
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+//Routes
+
+
+
+export default app;
