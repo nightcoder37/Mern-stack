1a1644252b518667ac8d967fe10806c62d5f0b37
Hassan-shah9 <hassan.thepearlsystem@gmail.com>
2025-01-11 18:01:20 +0700

    work on Route

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@ import express from "express";
 const app = express();
 
 //Routes
+// Http methods :- GET, POST , PUT , PATCH, DELETE
 
-
+app.get("/", (req, res, next) => {
+  res.json({ message: "Hello from the server" });
+});
 
 export default app;
